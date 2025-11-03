cc02ca7174624aa13b12e51455e582396c50846c
TBanda27 <tzbanda@outlook.com>
2025-11-03 10:13:02 +0000

    hello world controller

---
+++ src/main/java/com/restful_webservices/rest/helloworld/HelloWorldController.java
@@ -0,0 +1,25 @@
+package com.restful_webservices.rest.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello-world")
+    public String helloWorld(){
+        return "Hello World";
+    }
+
+    @GetMapping("/hello-world-bean")
+    public HelloWorldBean helloWorldBean(){
+
+        return new HelloWorldBean("Hello World Bean");
+    }
+
+    @GetMapping("/hello-world/{name}")
+    public HelloWorldBean helloWorld(@PathVariable("name") String name){
+        return new HelloWorldBean(String.format("Hello World %s", name));
+    }
+}
